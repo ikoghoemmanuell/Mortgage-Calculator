8ba2194212bd963a8587eacedc542b18e5e37c40
ikoghoemmanuell <oke.kogo@gmail.com>
2022-10-02 12:20:59 +0100

    added good spacing

--- Mortgage_Calculator.py
+++ Mortgage_Calculator.py
@@ -1,18 +1,18 @@
-# principal=1000000
-# interest=7.5
-# years=30
+#Ask the user for principal, yearly interest and years
+principal=float(input('What is your principal? \n'))
+yearlyInterest=float(input('What is your yearly interest? \n'))
+years=int(input('For how many years? \n'))
+print()
 
-principal=int(input('type principal'))
-interest=float(input('type interest'))
-years=int(input('type number of years'))
-
+# prepare variables for formulas
 p=principal
-i=(interest/100)/12
+i=(yearlyInterest/100)/12
 n=years*12
 
+#create formulas
 mortgage=p*(i*(1+i)**n)/((1+i)**n-1)
 totalAmount=mortgage*n
 
-print(f'For a {years}-year mortgage of ${principal} at an annual interest rate of {interest}% you pay ${mortgage} monthly')
-print()
-print(f'Total amount paid will be ${totalAmount}')
+#Print results
+print(f'For a {years}-year mortgage of ${principal:.2f} at an annual interest rate of {yearlyInterest}% you pay ${mortgage:.2f} monthly')
+print(f'Total amount paid will be ${totalAmount:.2f}\n')
